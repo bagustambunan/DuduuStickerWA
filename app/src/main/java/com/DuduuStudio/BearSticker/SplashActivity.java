4840a170fdf921b7b9bb6d44102ed500d4ecdb23
Muhammad Bagus Syahputra Tambunan <mhdbagus78@gmail.com>
2021-07-19 02:53:06 +0700

    Commit ke-1

---
+++ app/src/main/java/com/DuduuStudio/BearSticker/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.DuduuStudio.BearSticker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity  extends AppCompatActivity {
+
+    private int waktu_loading=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this).load(R.drawable.anim).into(imageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, EntryActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },waktu_loading);
+    }
+}
